89efab9b46880ae1402a0024e8b47c3024703e76
JM <kmicheli2@gmail.col>
2020-11-12 14:42:26 +0100

    Almost no change

--- Flowers/ViewController.swift
+++ Flowers/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("view controller func viewDidLoad")
+        print("view controller func func viewDidLoad")
     }
 
 
